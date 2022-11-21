ce08285d6f4fb9207c4f01dc82fcc30593416437
Utsav Basyal <utsabbasyal12@gmail.com>
2022-11-21 21:53:19 +0545

    added controller

---
+++ TodoListApi/Controllers/TodoListController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoListApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoListController : ControllerBase
+    {
+        public TodoListController()
+        {
+
+        }
+    }
+}
