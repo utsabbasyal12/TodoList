58b22a28c9763d5b56dd5ce974c64a19134efe10
Utsav Basyal <utsabbasyal12@gmail.com>
2022-11-21 21:39:07 +0545

    Added models and data folder

---
+++ TodoListApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoListApi.Models;
+
+namespace TodoListApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        
+        public DbSet<TodoList> todoLists { get; set; } 
+    }
+}
---
+++ TodoListApi/Models/TodoList.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoListApi.Models
+{
+    public class TodoList
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Task { get; set; }
+        public string? Description { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
